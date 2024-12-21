0ba5a6d6abb89c5a97942f5dd51e36ac9d516633
Abrar <ak3900051@gmail.com>
2024-12-21 12:45:38 +0530

    Add Homepage.jsx

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    host: true, // Enables listening on all IPs
-  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Adjust this if you use aliases for paths
+    },
+  },
 });
