fd9f796ba1ee390b1285518f18afb6885a914c82
Abrar <ak3900051@gmail.com>
2024-12-21 12:50:07 +0530

    Add Homepage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from './components/pages/LoginPage';
-import Homepage from './components/pages/Homepage';
+import Homepage from '@/components/pages/Homepage';
 import Navbar from './components/navbar';
 import Footer from './components/Footer';
 import AddToCartPage from './components/pages/Addtocart';
